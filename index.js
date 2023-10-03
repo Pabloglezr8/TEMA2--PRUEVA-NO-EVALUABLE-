b3819568952220d3af0500c749b1afa676d81d35
Pabloglezr8 <pabligoru99@gmail.com>
2023-10-03 10:35:22 +0200

    Hacemos la prueba no evaluable

---
+++ index.js
@@ -0,0 +1,12 @@
+const numero = prompt();
+
+if (numero === 0) {
+    console.log("EL NUMERO ES CERO")
+    alert("EL NUMERO ES CERO")
+} else if (numero > 0) {
+    console.log("EL NUMERO ES POSITIVO")
+    alert("EL NUMERO ES POSITIVO")
+} else{
+    console.log("EL NUMERO ES NEGATIVO")
+    alert("EL NUMERO ES NEGATIVO")
+}
